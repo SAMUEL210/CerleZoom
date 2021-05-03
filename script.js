e9798539043a4abd6fb927e9b8c3716e8a497e17
Samuel <samuel210@live.fr>
2021-05-03 21:37:18 +0200

    Cercle Zoom Utilisation de addEvenLister avec mouseenter et mouseleave

---
+++ script.js
@@ -0,0 +1,12 @@
+const cercle = document.querySelector(".cercle");
+
+cercle.addEventListener("mouseenter", () => {
+    if (!cercle.classList.contains("hover")) {
+        cercle.classList.add("hover");
+    }
+});
+cercle.addEventListener("mouseleave", () => {
+    if (cercle.classList.contains("hover")) {
+        cercle.classList.remove("hover");
+    }
+});
